aff1950d56d35e55472ad4e260edebbbce98db34
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-18 16:20:01 +0100

    9. Create a Component with Composition Now we will look at how we can compose multiple React components together. Imagine you are building an app and have created three components: a Navbar, Dashboard, and Footer.
    To compose these components together, you could create an App parent component which renders each of these three components as children. To render a component as a child in a React component, you include the component name written as a custom HTML tag in the JSX. For example, in the render method you could write:
    
    return (
     <App>
      <Navbar />
      <Dashboard />
      <Footer />
     </App>
    )
    When React encounters a custom HTML tag that references another component (a component name wrapped in < /> like in this example), it renders the markup for that component in the location of the tag. This should illustrate the parent/child relationship between the App component and the Navbar, Dashboard, and Footer.
    
    In the code editor, there is a simple functional component called ChildComponent and a class component called ParentComponent. Compose the two together by rendering the ChildComponent within the ParentComponent. Make sure to close the ChildComponent tag with a forward slash.
    
    Note: ChildComponent is defined with an ES6 arrow function because this is a very common practice when using React.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,44 +4,49 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-class MyComponent1 extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+// Child Component
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+// Parent Component
+class ParentComponent extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
   render() {
-    // Change code below this line
+    return (
+      <div>
+        <h1>I am the parent</h1>
+        <ChildComponent />
+      </div>
+    );
+  }
+}
+
+// Class Component Example
+class MyComponent1 extends React.Component {
+  render() {
     return (
       <div>
         <h1>Hello React!</h1>
       </div>
     );
-    // Change code above this line
   }
 }
 
-// Define the JSX constant first
+//  Define the JSX constant first
 const MyComponent = function () {
   // Change code below this line
-
   return <div>text</div>;
-
   // Change code above this line
 };
 
-const JSX2 = (
-  <div className="myDiv">
-    <div>
-      <h2>Welcome to React!</h2> <br />
-      <p>Be sure to close all tags!</p>
-      <hr />
-    </div>
-    <h1>Add a class to this div</h1>
-
-    <h1>Hello World</h1>
-    <p>Lets render this to the DOM</p>
-  </div>
-);
-// Add your code below this line
+// JSX Examples
 const JSX = <h1 className="h1">Hello JSX!</h1>;
 const JSX1 = (
   <div>
@@ -55,24 +60,40 @@ const JSX1 = (
     </ul>
   </div>
 );
+const JSX2 = (
+  <div className="myDiv">
+    <div>
+      <h2>Welcome to React!</h2> <br />
+      <p>Be sure to close all tags!</p>
+      <hr />
+    </div>
+    <h1>Add a class to this div</h1>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
+);
 
-// Render the application
+// Render All Components and JSX Together
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ParentComponent />
     <MyComponent1 />
     <MyComponent />
-    {JSX} {/* Render the JSX directly */}
-    {JSX1} {/* Render the JSX1 directly */}
+    {JSX}
+    {JSX1}
+    {JSX2}
     <App />
-    {/* Remove <JSX /> here, as it is not a valid component */}
   </React.StrictMode>
 );
 
-root.render(JSX2);
+// Start Measuring Performance
+
+// root.render(JSX2);
 // root.render(JSX2, document.getElementById("challenge-node"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
