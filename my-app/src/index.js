eca31962d2d24ee8d2584aec9268ba8daaf0e67a
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-19 13:59:55 +0100

    17. Override Default Props The ability to set default props is a useful feature in React. The way to override the default props is to explicitly set the prop values for a component.
    The ShoppingCart component now renders a child component Items. This Items component has a default prop quantity set to the integer 0. Override the default prop by passing in a value of 10 for quantity.
    
    Note: Remember that the syntax to add a prop to a component looks similar to how you add HTML attributes. However, since the value for quantity is an integer, it won't go in quotes but it should be wrapped in curly braces. For example, {100}. This syntax tells JSX to interpret the value within the braces directly as JavaScript.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,18 +4,41 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const ShoppingCart = (props) => {
-  return (
-    <div>
-      <h1>Shopping Cart Component</h1>
-    </div>
-  );
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>;
 };
-// Change code below this line
 
-ShoppingCart.defaultProps = {
-  items: 0,
+Items.defaultProps = {
+  quantity: 0,
 };
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    {
+      /* Change code below this line */
+    }
+    return <Items quantity={10} />;
+    {
+      /* Change code above this line */
+    }
+  }
+}
+
+// const ShoppingCart = (props) => {
+//   return (
+//     <div>
+//       <h1>Shopping Cart Component</h1>
+//     </div>
+//   );
+// };
+// // Change code below this line
+
+// ShoppingCart.defaultProps = {
+//   items: 0,
+// };
 
 const List = (props) => {
   //  {/* Change code below this line */}
