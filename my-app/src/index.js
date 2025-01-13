3e7b01a574ecca56fc8e8d082a532a8a83f746f3
Voichita Maria <sabo_voichita@yahoo.com>
2025-01-13 20:08:18 +0100

    38. Render with an If-Else Condition Another application of using JavaScript to control your rendered view is to tie the elements that are rendered to a condition. When the condition is true, one view renders. When it's false, it's a different view. You can do this with a standard if/else statement in the render() method of a React component.
    MyComponent contains a boolean in its state which tracks whether you want to display some element in the UI or not. The button toggles the state of this value. Currently, it renders the same UI every time. Rewrite the render() method with an if/else statement so that if display is true, you return the current markup. Otherwise, return the markup without the h1 element.
    
    Note: You must write an if/else to pass the tests. Use of the ternary operator will not pass here.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,6 +5,38 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import PropTypes from "prop-types";
 
+class MyComponent11 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: true,
+    };
+    this.toggleDisplay = this.toggleDisplay.bind(this);
+  }
+  toggleDisplay() {
+    this.setState((state) => ({
+      display: !state.display,
+    }));
+  }
+  render() {
+    // Change code below this line
+    if (this.state.display === true) {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+          <h1>Displayed!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleDisplay}>Toggle Display</button>
+        </div>
+      );
+    }
+  }
+}
+
 const inputStyle = {
   width: 235,
   margin: 5,
@@ -844,6 +876,7 @@ const JSX2 = (
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <MyComponent11 />
     <MagicEightBall />
     <Colorful1 />
     <Colorful />
